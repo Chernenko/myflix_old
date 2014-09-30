6f286157dffd7b5cf0c45ab462d1b7725114b5e8
lana chernenko <chernenko.lana@gmail.com>
2014-09-30 11:15:47 -0700

    Seeding Data

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: "Switched at Birth", description: "Switched at Birth is an American teen,family drama television series that premiered on ABC Family on June 6, 2011, at 9:00 ET/PT.The one-hour scripted drama is set in the Kansas City metropolitan area, and revolves around two teenagers who were switched at birth and grew up in very different environments: one in the old money suburb of Mission Hills, Kansas, and the other in working-class East Riverside, Missouri. According to ABC Family, it is the first mainstream television series to have multiple deaf and hard-of-hearing series regulars and scenes shot entirely in American Sign Language (ASL)", small_cover_url: "switched_at_birth.jpg", large_cover_url: "switched_at_birth_large.jpg")
+Video.create(title: "The Walking Dead", description: "The Walking Dead is an American post-apocalyptic horror drama television series developed by Frank Darabont. It is based on the comic book series of the same name by Robert Kirkman, Tony Moore, and Charlie Adlard. The series stars Andrew Lincoln as sheriff's deputy Rick Grimes,[3] who awakens from a coma to find a post-apocalyptic world dominated by flesh-eating zombies. He sets out to find his family and encounters many other survivors along the way.", small_cover_url: "walking_dead.jpg", large_cover_url: "walking_dead_large.jpg")
+Video.create(title: "Monk", description: "Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk. It originally ran from 2002 to 2009 and is primarily a police procedural series, and also exhibits comic and dramatic tones in its exploration of the main characters' personal lives.", small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg")
+
