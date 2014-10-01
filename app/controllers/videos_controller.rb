5fdf1134bbdfa2c7d444740dbaee51825340d468
lana chernenko <chernenko.lana@gmail.com>
2014-09-30 23:33:40 -0700

    Categories

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,14 @@
 class VideosController < ApplicationController
-
+  before_action :set_video, only: [:show]
   def index
     @video = Video.all
     end
 
   def show
-    #binding.pry
-  end
 
+  end
+  def set_video
+    @video = Video.find(params[:id])
+
+  end
 end
